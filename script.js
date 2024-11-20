5f2febd26f64150b93b98b29df4105b216899763
ahmedwsiim <mtmind4mo@gmail.com>
2024-11-20 22:30:13 +0500

    v.1.6

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ document.getElementById('myForm').addEventListener('submit', function (event)
         event.preventDefault(); 
     }
 });
+
+
+
+var mybutton = document.getElementById("backToTop");
+
+        // When the user scrolls down 100px from the top of the document, show the button
+        window.onscroll = function() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 100) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        };
+
+        // When the user clicks on the button, scroll to the top of the document
+        mybutton.onclick = function() {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        };
+
