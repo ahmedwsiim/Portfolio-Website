0ad9c887c829ca017bfc9ec139d31bd61fde2ad4
ahmedwsiim <mtmind4mo@gmail.com>
2024-11-20 23:06:04 +0500

    v.1.6.1

--- script.js
+++ script.js
@@ -11,21 +11,35 @@ document.getElementById('myForm').addEventListener('submit', function (event)
     }
 });
 
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
 
+// var mybutton = document.getElementById("backToTop");
 
-var mybutton = document.getElementById("backToTop");
+//         // When the user scrolls down 100px from the top of the document, show the button
+//         window.onscroll = function() {
+//             if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 100) {
+//                 mybutton.style.display = "block";
+//             } else {
+//                 mybutton.style.display = "none";
+//             }
+//         };
 
-        // When the user scrolls down 100px from the top of the document, show the button
-        window.onscroll = function() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 100) {
-                mybutton.style.display = "block";
-            } else {
-                mybutton.style.display = "none";
-            }
-        };
-
-        // When the user clicks on the button, scroll to the top of the document
-        mybutton.onclick = function() {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        };
+//         // When the user clicks on the button, scroll to the top of the document
+//         mybutton.onclick = function() {
+//             window.scrollTo({ top: 0, behavior: 'smooth' });
+//         };
 
