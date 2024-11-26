dcf92e623e0b453d71963ef7b269d7b07341c212
ahmedwsiim <mtmind4mo@gmail.com>
2024-11-26 20:04:20 +0500

    v.1.7.3

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ document.getElementById('myForm').addEventListener('submit', function (event)
     }
 });
 
-$(document).ready(function(){
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('#back-to-top').fadeIn();
-			} else {
-				$('#back-to-top').fadeOut();
-			}
-		});
-		$('#back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 400);
-			return false;
-		});
-});
+// $(document).ready(function(){
+// 	$(window).scroll(function () {
+// 			if ($(this).scrollTop() > 50) {
+// 				$('#back-to-top').fadeIn();
+// 			} else {
+// 				$('#back-to-top').fadeOut();
+// 			}
+// 		});
+// 		$('#back-to-top').click(function () {
+// 			$('body,html').animate({
+// 				scrollTop: 0
+// 			}, 400);
+// 			return false;
+// 		});
+// });
 
 
